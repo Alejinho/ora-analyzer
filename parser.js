2ea3e88f33f5ad05e55118174f4471b275a1751c
Alejandro Rivera <alejo@lapix.com.co>
2023-08-02 23:41:49 -0500

    Add a new parser for the SQL queries

--- parser.js
+++ parser.js
@@ -1,31 +1,74 @@
+import nodeSQL from 'node-sql-parser';
+import plsqlParser from 'oracle-plsql-parser';
+
 import antlr4 from 'antlr4';
 import PlSQLGrammarLexer from './storage/grammar/PlSqlLexer.js';
 import PlSQLGrammarParser from './storage/grammar/PlSqlParser.js';
 import PlSQLGrammarListener from './storage/grammar/PlSqlParserListener.js';
 
 class TablesFinderListener extends PlSQLGrammarListener {
-  tables = [];
+  tables = {};
 
   clearTables() {
-    this.tables = []
+    this.tables = {}
+  }
+
+  getTables() {
+    return Object.keys(this.tables)
   }
 
   exitTable_ref_aux_internal_one(ctx) {
-    this.tables.push(ctx.getText())
+    this.tables[ctx.getText().toUpperCase()] = null
   }
 }
 
-export function getTables(input) {
+export function getTablesFromPackage(input) {
   const chars = new antlr4.InputStream(input);
   const lexer = new PlSQLGrammarLexer(chars);
   const tokens = new antlr4.CommonTokenStream(lexer);
   const parser = new PlSQLGrammarParser(tokens);
 
-  parser.buildParseTrees = true;
+  // parser.buildParseTrees = true;
   const tree = parser.sql_script(); 
 
   const finder = new TablesFinderListener()
   antlr4.tree.ParseTreeWalker.DEFAULT.walk(finder, tree);
 
-  return finder.tables
+  return finder.getTables()
+
+  // const ast = plsqlParser.default(input)
+  // console.log(ast)
+  // console.log(plsqlParser.getInterpretation(ast.cst))
+}
+
+export function getTablesFromSQL(input) {
+  const parser = new nodeSQL.Parser();
+  const list = parser.tableList(
+    input.toUpperCase()
+
+      // Sanitize the default varchar.
+      .replace(/VARCHAR2/, 'VARCHAR')
+
+      // Sanitize DBLinks, transform 'remote@users' into 'DBLINK_remote.users', later
+      // we'll use this prefix to check the object type.
+      .replace(/([\w\d]+)@([\w\d]+)/, 'DBLINK_$1.$2')
+  );
+
+  return list.map(table => {
+    const [type, db, name] = table.split('::')
+    const isDBLink = db && db.startsWith('DBLINK_')
+
+    if (isDBLink) {
+      return {
+        type: 'DBLINK',
+        name: db.replace('DBLINK_', '').toUpperCase(),
+        table: name.toUpperCase(),
+      }
+    }
+
+    return {
+      type: 'TABLE',
+      name: name.toUpperCase(),
+    }
+  })
 }
--- tests/tables-finder.js
+++ tests/tables-finder.js
@@ -1,19 +1,36 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import { describe, it } from 'node:test';
 import assert from 'node:assert/strict'
-import { getTables } from '../parser.js'
+import { getTablesFromSQL, getTablesFromPackage } from '../parser.js'
 
-describe('Getting tables from queries', () => {
+describe('Getting tables from SQL', () => {
 
   it('should get the table in a simple query', () => {
     const input = `SELECT * FROM users;`
-    const tables = getTables(input)
-    assert.deepEqual(tables, ['users'])
+    const tables = getTablesFromSQL(input)
+    assert.deepEqual(tables, [{type: 'TABLE', name: 'USERS'}])
+  })
+
+  it('should get the table in a simple query with a DBLink', () => {
+    const input = `SELECT * FROM remote@users;`
+    const tables = getTablesFromSQL(input)
+    assert.deepEqual(tables, [
+      {
+        type: 'DBLINK',
+        name: 'REMOTE',
+        table: 'USERS',
+      },
+    ])
   })
 
   it('should get the tables from the joined tables', () => {
     const input = `SELECT * FROM users u INNER JOIN employees e ON e.user_id = u.id;`
-    const tables = getTables(input)
-    assert.deepEqual(tables, ['users', 'employees'])
+    const tables = getTablesFromSQL(input)
+    assert.deepEqual(tables, [
+      {type: 'TABLE', name: 'USERS'},
+      {type: 'TABLE', name: 'EMPLOYEES'},
+    ])
   })
 
   it('get tables from nested queries', () => {
@@ -21,8 +38,29 @@ describe('Getting tables from queries', () => {
       users.id,
       (SELECT deleted_at FROM log WHERE users.id = log.owner_id) as deleted_at
     FROM users;`
-    const tables = getTables(input)
-    assert.deepEqual(tables, ['log', 'users'])
+    const tables = getTablesFromSQL(input)
+    assert.deepEqual(tables, [
+      {type: 'TABLE', name: 'LOG'},
+      {type: 'TABLE', name: 'USERS'},
+    ])
+  })
+
+  it('get tables from DDL', () => {
+    const input = `ALTER TABLE users ADD COLUMN name VARCHAR2(255) NOT NULL DEFAULT 'default-value';`
+    const tables = getTablesFromSQL(input)
+    assert.deepEqual(tables, [
+      {type: 'TABLE', name: 'USERS'},
+    ])
   })
 
 })
+
+// describe('Getting tables from packages', () => {
+
+//   it('should parse a simple package', async () => {
+//     const input = await fs.readFile(path.join(process.cwd(), 'tests/examples/employees.sql'))
+//     const tables = getTablesFromPackage(input.toString())
+//     assert.deepEqual(tables, ['EMPLOYEES'])
+//   })
+
+// })
